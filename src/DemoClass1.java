52acc5e1c1abe73fb2f38395a57bde6f1b86a82f
virendra-ravat <viruravat005@gmail.com>
2023-03-28 11:00:06 +0530

    first commit

---
+++ src/DemoClass1.java
@@ -0,0 +1,13 @@
+public class DemoClass1 {
+
+    public static void main (String args[]){
+
+        System.out.println("i am reading.");
+        System.out.println("you are waiting for me .");
+        System.out.println("let him go.");
+        System.out.println("we are going.");
+        System.out.println("i wants 10 rupees.");
+        System.out.println("he should dead");
+
+    }
+}
